fa3dc19414e6efe18efbe666951dc8b955d78d5d
Mohammad Sameer <sammusmarty563@gmail.com>
2025-12-28 19:02:17 +0530

    Initial portfolio deployment

---
+++ script.js
@@ -0,0 +1,34 @@
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+                observer.unobserve(entry.target); // animate once
+            }
+        });
+    },
+    {
+        threshold: 0.15
+    }
+);
+
+reveals.forEach(el => observer.observe(el));
+
+
+// Device & browser infoF
+document.getElementById("device").value =
+    /Mobi|Android/i.test(navigator.userAgent) ? "Mobile" : "Desktop";
+
+document.getElementById("browser").value = navigator.userAgent;
+
+
+document.getElementById("os").value = navigator.platform;
+
+document.getElementById("timezone").value =
+    Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+document.getElementById("screen").value =
+    `${window.screen.width}x${window.screen.height}`;
+
